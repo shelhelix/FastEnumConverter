3b7a66d60968d36908b3d45aa24fd0e050ee00aa
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-11-01 11:35:53 +0700

    simple utility class for the fast conversion from the enum to the string and vice versa

---
+++ FastEnumConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Benchmark {
+	public class FastEnumConverter<T> where T : struct, Enum  {
+		static Dictionary<T, string> _enumToStringConverter = new Dictionary<T, string>();
+		static Dictionary<string, T> _stringToEnumConverter = new Dictionary<string, T>();
+
+		public static string ConvertToString(T value) {
+			if ( _enumToStringConverter.TryGetValue(value, out var res) ) {
+				return res;
+			}
+			var str = value.ToString();
+			if ( string.IsNullOrEmpty(str) ) {
+				return str;
+			}
+			_enumToStringConverter.Add(value, str);
+			if ( !_stringToEnumConverter.ContainsKey(str) ) {
+				_stringToEnumConverter.Add(str, value);
+			}
+			return str;
+		}
+
+		public static T ConvertToEnum(string str) {
+			if ( _stringToEnumConverter.TryGetValue(str, out var value) ) {
+				return value;
+			}
+			if (Enum.TryParse(str, out T res)) {
+				_stringToEnumConverter.Add(str, res);
+				if ( !_enumToStringConverter.ContainsKey(res) ) {
+					_enumToStringConverter.Add(res, str);
+				}
+				return res;
+			}
+			return default;
+		}
+		
+	}
+}
